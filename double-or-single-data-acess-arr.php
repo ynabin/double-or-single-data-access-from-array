9bd86bbcd9fe5a241eb9079847bb29ddf81770b7
Nabin Yadav <110272675+ynabin@users.noreply.github.com>
2024-08-05 08:59:12 -0700

    Add files via upload
    double or single data access from array

---
+++ double-or-single-data-acess-arr.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$cars = array("brand" => "Ford", "model" => "Mustang", "year" => 1964);
+echo $cars["brand"];
+echo "<br>";
+echo $cars['brand'];
+?>  
+
+</body>
+</html>
